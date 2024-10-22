0a159067dee1d2b37688c4fcea844c4a8592c05a
Raoul Zebisch <raoul.zebisch@uni-a.de>
2024-10-22 17:33:34 +0200

    Add DT Operation Mode

--- FSR/DigitalTwin/Client/Unity/FSR.DigitalTwin.Client/Assets/FSR/Scripts/Workspace/Virtual/Component/DigitalTwinComponentBase.cs
+++ FSR/DigitalTwin/Client/Unity/FSR.DigitalTwin.Client/Assets/FSR/Scripts/Workspace/Virtual/Component/DigitalTwinComponentBase.cs
@@ -1,33 +1,40 @@
 using System.Threading.Tasks;
+using FSR.DigitalTwin.Client.Unity.Workspace.Digital.Core;
 using FSR.DigitalTwin.Client.Unity.Workspace.Digital.Interfaces;
+using FSR.DigitalTwin.Client.Unity.Workspace.Virtual.Actor;
+using UnityEngine;
 
 namespace FSR.DigitalTwin.Client.Unity.Workspace.Virtual.Component {
 
-    public abstract class DigitalTwinComponentBase : IDigitalTwinEntityComponent
+    public abstract class DigitalTwinComponentBase : MonoBehaviour, IDigitalTwinEntityComponent
     {
-        public IDigitalTwinEntity DigitalTwinEntity { get => throw new System.NotImplementedException(); init => throw new System.NotImplementedException(); }
-        public string Id { get => throw new System.NotImplementedException(); init => throw new System.NotImplementedException(); }
+        [SerializeField] private string _id = "mycomponent";
+        [SerializeField] private DigitalTwinActorBase _actor;
+        private bool _hasConnection = false;
 
-        public bool HasConnection => throw new System.NotImplementedException();
+        public IDigitalTwinEntity DigitalTwinEntity { get => _actor; init => _actor = null; }
+
+        public string Id { get => _id; init => _id = "mycomponent"; }
+        public bool HasConnection => _hasConnection;
 
         public T GetProperty<T>(string prop)
         {
-            throw new System.NotImplementedException();
+            return DigitalWorkspace.Instance.Entities.GetComponentProperty<T>(_id, prop);
         }
 
-        public Task<T> GetPropertyAsync<T>(string prop)
+        public async Task<T> GetPropertyAsync<T>(string prop)
         {
-            throw new System.NotImplementedException();
+            return await DigitalWorkspace.Instance.Entities.GetComponentPropertyAsync<T>(_id, prop);
         }
 
         public bool SetProperty<T>(string prop, T value)
         {
-            throw new System.NotImplementedException();
+            return DigitalWorkspace.Instance.Entities.SetComponentProperty(_id, prop, value);
         }
 
-        public Task<bool> SetPropertyAsync<T>(string prop, T value)
+        public async Task<bool> SetPropertyAsync<T>(string prop, T value)
         {
-            throw new System.NotImplementedException();
+            return await DigitalWorkspace.Instance.Entities.SetComponentPropertyAsync(_id, prop, value);
         }
     }
 
